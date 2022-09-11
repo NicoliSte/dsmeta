8d710428227880014e69c0f7aa16277f10b4587a
Nicoli <nickstefan4@gmail.com>
2022-09-11 00:20:53 -0300

    atualizando2

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
         Desenvolvido por 
-        <a href="https://www.instagram.com/nicolii.s/" >Nicoli Stefani</a>
+        <a href="https://www.instagram.com/nicolii.s/ " > @Nicoli Stefani</a>
         </p>
         </div>
     </header>
