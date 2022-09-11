a849b7832f75e36b72f61c9d33f8e085bf8e25d9
Nicoli <nickstefan4@gmail.com>
2022-09-10 23:43:33 -0300

    Toast

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,9 +18,9 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        const dmin = minDate.toISOString().slice(0,10);
-        const dmax = maxDate.toISOString().slice(0,10);
-        
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
@@ -65,9 +65,9 @@ function SalesCard() {
                     <tbody>
                         {sales.map(sale => {
                             return (
-                                <tr key= {sale.id}>
+                                <tr key={sale.id}>
                                     <td className="show992">{sale.id}</td>
-                                    <td className="show576">{new Date (sale.date).toLocaleDateString()}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                                     <td>{sale.sellerName}</td>
                                     <td className="show992">{sale.visited}</td>
                                     <td className="show992">{sale.deals}</td>
